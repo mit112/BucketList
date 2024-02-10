590ca83126f408d304f3346386677bdf0df95fbf
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-02-10 13:45:01 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Mit Sheth on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
